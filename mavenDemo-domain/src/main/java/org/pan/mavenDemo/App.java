6031161626ca0fd4e5b8b9e07adb12379cc03d98
panmingzhi <154341736@qq.com>
2014-03-19 22:05:43 +0800

    Initial commit

---
+++ mavenDemo-domain/src/main/java/org/pan/mavenDemo/App.java
@@ -0,0 +1,13 @@
+package org.pan.mavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
